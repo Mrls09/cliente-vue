03541ab8e0739bff059a0c7fc95f1b4b2648e8a6
ivans <20213tn073@utez.edu.mx>
2024-01-13 08:47:59 -0600

    Update

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div id="app">
+    <div>
+      <b-navbar type="dark" variant="dark" fixed="top">
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown text="User" right>
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+    <b-container>
+      <div>
+        <b-button>Button</b-button>
+        <b-button variant="danger">Button</b-button>
+        <b-button variant="outline-primary">Button</b-button>
+        <b-sidebar id="sidebar-border" sidebar-class="border-right border-danger v-b-toggle" visible="true"
+          no-header-close="false">
+          <div class="px-3 py-2">
+            <p>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+              in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            </p>
+            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+          </div>
+        </b-sidebar>
+      </div>
+    </b-container>
+
+  </div>
+</template>
+
+<style scoped></style>
