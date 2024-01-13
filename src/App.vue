1fcb20b324f2485f16ceb873c019558788fc5071
ivans <20213tn073@utez.edu.mx>
2024-01-13 09:07:21 -0600

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <div id="app">
-    <div>
-      <b-navbar type="dark" variant="dark" fixed="top">
-        <b-navbar-nav>
-          <b-nav-item href="#">Home</b-nav-item>
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown text="User" right>
-            <b-dropdown-item href="#">Account</b-dropdown-item>
-            <b-dropdown-item href="#">Settings</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-navbar>
+    <div class="navbar">
+      <p>Navbar</p>
     </div>
-    <b-container>
-      <div>
-        <b-button>Button</b-button>
-        <b-button variant="danger">Button</b-button>
-        <b-button variant="outline-primary">Button</b-button>
-        <b-sidebar id="sidebar-border" sidebar-class="border-right border-danger v-b-toggle" visible="true"
-          no-header-close="false">
-          <div class="px-3 py-2">
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-              in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-            </p>
-            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
-          </div>
-        </b-sidebar>
+    <div class="container">
+      <div class="sidebar">
+        <p>Sidebar</p>
       </div>
-    </b-container>
-
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.container {
+  display: flex;
+}
+
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #3498db;
+  padding: 10px;
+  height: 60px;
+  color: #fff;
+}
+
+.sidebar {
+  width: 200px;
+  height: 100%;
+  background-color: #2c3e50;
+  color: #fff;
+  position: fixed;
+  left: 0;
+  top: 60px; /* Ajusta según el tamaño de tu navbar */
+  overflow-y: auto;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+</style>
